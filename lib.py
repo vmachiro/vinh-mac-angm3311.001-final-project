b36961bdede7f4da168d786d2fedec7913304010
Vinh Mac <hhm220000@utdallas.edu>
2025-10-25 13:17:10 -0500

    pseudocode

---
+++ lib.py
@@ -0,0 +1,7 @@
+import maya.cmds as cmds
+
+# declare class of generated model
+# define init and the basic parameters that'll be changed (5-10)
+# define methods that would be useful for calculation
+# define method that builds the object
+# define main
