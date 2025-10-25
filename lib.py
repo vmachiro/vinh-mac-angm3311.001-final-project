aadc90d8ca7a4e972ca54f6dfd5720ef34484760
Vinh Mac <hhm220000@utdallas.edu>
2025-10-25 14:23:06 -0500

    implement mkhousebody

--- lib.py
+++ lib.py
@@ -1,26 +1,28 @@
 import maya.cmds as cmds
 
-# declare class of generated model
 # house model is specifically for environment bgs, not for level exploration
 class House():
-    # define init and the basic parameters that'll be changed (5-10)
     def __init__(self):
         self.number_of_floors = 1        
         self.wall_height = 8 # this is impacted by number of floors
-        self.roof_height = 4 # this should be something that can be 0 and still work, for stuff like skyscrapers or apartments with flat roofs. maybe make it toggle-able between triangle and cube???
+        self.house_width = 8
+        self.roof_height = 4 # this should be something that can be 0 and still work, for stuff like skyscrapers or apartments with flat roofs. 
         self.number_of_windows = 4
         self.window_height = 2
         self.window_width = 2
-        self.number_of_doors = 1 # will there be double doors or will it just be a single door placed front and back??
+        self.number_of_doors = 1 
         self.door_height = 2
         self.door_width = 1
-
-    # define methods that would be useful for calculation
     
-    def get_window_height_from_base():
+    def get_window_height_from_base(self):
         # TODO:
         # If more than one floor: Find lowest point of the house
         # Place window halfway through the height of the house
+        pass
+
+    def get_center_of_wall(self):
+        # Find 0.5 of the width
+        # used in door and window placement
         pass
     
     # define the method that makes the house body
@@ -28,6 +30,15 @@ class House():
         print("Making your house!")
         # TODO:
         # Use polycube to make the main body based on wall_height and number_of_floors
+        xform, shape = cmds.polyCube(height= self.wall_height,
+                                    width = self.house_width,
+                                    depth = self.wall_height/self.house_width,
+                                    name = "housebody")
+        
+        cmds.makeIdentity(xform, apply=True, translate=True, rotate=True, 
+                          scale=True, normal=False, preserveNormals=True)
+        
+        return xform
 
     def mkwindows(self):
         print("Making windows...")
@@ -66,16 +77,27 @@ class House():
     def build(self):
         """Builds the house"""
         # TODO:
+        house_things=[]
         # Create the house body
+        housebody = self.mkhousebody()
+        house_things.append(housebody)
         # Check if there is a roof.
-        # IF so, Create the roof
-        # Place the windows (Do windows have to be made in a separate method? Aw man)
+        if self.roof_height != 0:
+            # IF ROOF: Check what kind of roof. (DO THIS LATER)
+            # Create the roof (set to flatroof for now bc easier)
+            houseroof = self.mkhouseflatroof()
+            house_things.append(houseroof)
+        # Create the front door
+        housedoor1 = self.mkdoors()
+        house_things.append(housedoor1) # Figure out multiple doors later
         # Place the front door 
+        cmds.xform(xform, translation = [self.get_center_of_wall,0,0])
         # If more than one door, place second door
+        # Place the windows 0.4 of the door     
+        cmds.group(house_things, name="House1") 
+        pass 
     
     
-# define main
-
 if __name__ == "__main__":
     house1 = House()
     house1.wall_height = 4
