4edae14ad5683bead62841ac994a27ad07f5e99f
Vinh Mac <hhm220000@utdallas.edu>
2025-11-04 21:21:49 -0600

    added window number customizability

--- houselib.py
+++ houselib.py
@@ -56,8 +56,14 @@ class HouseGenWin(QtWidgets.QDialog):
         self._add_roof_height()
         self._add_wall_height()
         self._add_floors()        
+        self._add_windows()
         self._add_doors()
         self.main_layout.addLayout(self.form_layout)
+
+    def _add_windows(self):
+        self.number_of_windows_spnbox = QtWidgets.QSpinBox()
+        self.number_of_windows_spnbox.setValue(2)
+        self.number_of_windows_spnbox.addRow("Window Number", self.number_of_windows_spnbox)
 
     def _add_floors(self):
         self.number_of_floors_spnbox = QtWidgets.QSpinBox()
